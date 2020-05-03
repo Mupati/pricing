212358fe102fc2a3b564f91b906942595c438213
Mupati <scientificgh@gmail.com>
2020-05-03 12:21:45 +0000

    Initial Commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Choose Your Planc</h1>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
---
+++ src/components/Price.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Bronze</h1>
+    <p>GHs 200.00/month</p>
+    <button>Subscribe</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "price",
+  props: {},
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped></style>
